2d7ac7b33d1024fcbc1dac1a61d244a54bb72f81
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-09-14 14:45:09 -0300

    commit

---
+++ Aulas/tipos-de-dados/script.js
@@ -0,0 +1,10 @@
+let nome = "Gabriel"; //tipo string,basicamente quando vc junta varias letras ou seja texto
+
+let idade = 17; // tipo number ou número
+
+let estaAprovado = true; //tipo boolean (verdadeiro ou falso)
+
+let sobrenome; //Undefined ou seja está sem definição
+
+let corSelecionada = null //Redefinir um valor 
+
