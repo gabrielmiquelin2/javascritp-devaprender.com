b599059fa428ce81db3ccb1f1ade68999cca110c
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-09-14 15:00:57 -0300

    commit

---
+++ Aulas/tipagem-dinamica/scirpt.js
@@ -0,0 +1,6 @@
+//javascript é uma linguagem dinâmica e pode ser alterada a qualquer momento
+let nome = "Rafael";
+let idade = 32;
+let estaAprovado = true;
+let sobrenome = undefined;
+let corSelecionado = null;
