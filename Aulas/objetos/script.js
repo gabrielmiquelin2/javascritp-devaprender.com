84cafeb2a8cf31700450460ae59b89cc0b41cfc3
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-09-14 15:15:11 -0300

    commit

---
+++ Aulas/objetos/script.js
@@ -0,0 +1,22 @@
+//Objetos vai basicamente juntar infiomações em um lugar só
+//representado pelas chaves {}
+
+//Sem utilizar "objetos"
+    // let nome = "Rafael";
+    // let idade = 32;
+    // let estaAprovado = true;
+    // let sobrenome = undefined;
+    // let corSelecionado = null;
+    // console.log(nome)
+
+/////////////////////////////////////////////////////////////////////////////////
+
+//utilizando objetos
+//objeto pessoa
+let pessoa = {
+  nome: "Rafael",
+  idade: 32,
+  estaAprovado: true,
+  sobrenome: "Ferreira"
+};
+console.log(pessoa);
