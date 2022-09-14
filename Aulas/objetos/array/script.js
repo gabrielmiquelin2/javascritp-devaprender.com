96d56f64fcdcbae8a345986e8183509d56a626fb
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-09-14 15:23:15 -0300

    commit

---
+++ Aulas/objetos/array/script.js
@@ -0,0 +1,6 @@
+//Arrays de forma simplificada é um conjunto de dados que poder ser acessado a partir de um indice
+
+let familia = [true,43, 'Gabriel',17];
+console.log(familia.length)//o "length" mostra o tamanho do array ou seja quantos dados 
+                            //tem dentro dele  
+console.log(familia[0])//acessei o indice 2 
