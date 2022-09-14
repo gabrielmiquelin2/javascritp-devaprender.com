824efbb0edffef3c1889b4baf8ab036cd95ad445
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-09-14 17:17:06 -0300

    commit

---
+++ Aulas/operadores/script.js
@@ -0,0 +1,98 @@
+                //TIPOS DE OPERADORES
+
+//Operadores Artimétricos ou Matemátiocs
+
+//Opereadores de Atribuição
+
+//Operadores de Comparação
+
+//Operadores Lógicos
+
+//Operadores Bitwise
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+
+
+              //OPERADORES ARITMÉTRICOS OU MATEMÀTICO 
+      // let salario = 100;
+      // + , - , * , / , **
+
+      // console.log(salario + salario);
+      // console.log(salario - salario);
+      // console.log(salario * salario);
+      // console.log(salario / salario);
+      // console.log(salario ** salario); //numero vezes ele mesmo
+      // console.log(5 ** 5);
+
+//incremento ++ adciona 1 ao seu valor
+      //let idade = 18;
+      // console.log(idade++);
+      // console.log(idade)
+      //Para não escrever console 2 vezes
+      //console.log(++idade)
+
+//decremento -- subrai 1 ao seu valor
+      //let idade = 20
+      //console.log(idade--);
+      // console.log(idade)
+      //Para não escrever console 2 vezes
+      //console.log(--idade)
+
+     
+      
+///////////////////////////////////////////////////////////////////////////////////      
+
+
+               //OPERADORES DE ATRIBUIÇÂO     
+      //let valorTecladoGamer = 100;
+      //valorTecladoGamer = valorTecladoGamer + valorTecladoGamer
+      //forma mais rpida de fazer
+      //valorTecladoGamer += valorTecladoGamer
+      // console.log(valorTecladoGamer);
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+
+
+
+                //OPERADORES DE IGUALDAE
+      //igualdade valor e tipo
+      console.log(1 === 1);
+      console.log('1' === 1);//o valor é o mesmo!MAS o tipo NÃO!
+
+      //iguldade
+      console.log(1 == 1);//Ele apenas vai ver se é o mesmo valor
+      console.log('1' == 1);//Ele apenas vai ver se é o mesmo valor
+
+
+
+///////////////////////////////////////////////////////////////////////////////////      
+
+
+                  //OPERADORES TERNÁRIOS
+      //È uma forma mais simples ao invés de ficar usando "if" toda hora
+
+      //Imagine a seguinde situação
+      //Tem um cliente se ele tiver mais que 100 pontos ele é premium,caso contrário comum
+
+      let pontos = 200;
+      let tipo = pontos > 100 ? 'premium' : 'comum';
+      console.log(tipo)
+
+
+ 
+///////////////////////////////////////////////////////////////////////////////////     
+
+
+
+                  //OPERADORES LÓGICOS
+      //São usados para tomar decisões em condições multiplas
+      //Em JS temos 3 operadores
+
+      //Operador lógico E representado pelo &&
+      //Retorna TRUE se os dois operando forem TRUE por exemplo:
+      console.log(true && true);
