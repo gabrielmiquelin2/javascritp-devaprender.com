c0d4563654c019932c29e083a7e98df40ed05804
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-09-14 14:39:16 -0300

    commit

---
+++ Aulas/variáveis/aula.js
@@ -0,0 +1,19 @@
+// let idade = 5;
+// console.log(idade);
+// let altura =180;
+// console.log(altura);
+
+//utilizar o "camelCase"
+//ou seja primeira letra da variavel minuscula e a 
+//segunda palavra caso precise maiuculo
+//Exemplo: let nomeCompleto = ...
+
+//usando variavel "let"onde posso redefinir seu valor
+let valorIngressoAdulto = 30;
+valorIngressoAdulto = 40;
+console.log(valorIngressoAdulto);
+///////////////////////////////////////////////////////////////////////////////
+
+//utilizando a variável "const" não consigo redefinr seu valor pois ela é uma palavra reservada!
+const valorIngressoCrianca = 20
+console.log(valorIngressoCrianca)
