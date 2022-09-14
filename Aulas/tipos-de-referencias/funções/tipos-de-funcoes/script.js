d68315a866b73fdcacbefefde0be960cb41e7b33
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-09-14 16:42:48 -0300

    Update script.js

---
+++ Aulas/tipos-de-referencias/funções/tipos-de-funcoes/script.js
@@ -0,0 +1,17 @@
+//Existem 2 tipos de funções
+//A primeria Realiza uma tarefa e não devolve nada
+
+function dizerNome(){
+  console.log("Gabriel")
+};
+dizerNome();
+
+//A segunda faz um cálculo ou oeração e retorna algo
+function MultiplicarPorDois(valor){
+  return valor * 2;
+}
+ console.log(MultiplicarPorDois(5));
+
+ //UTILANDO VARIÁVIEL
+  // let resultado = MultiplicarPorDois (5);
+  // console.log(resultado);
