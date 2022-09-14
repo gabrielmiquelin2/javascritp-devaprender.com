461363c4887ab66cb555da1edfdaa080472ff52d
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-09-14 15:56:15 -0300

    commit

---
+++ Aulas/objetos/funções/script.js
@@ -0,0 +1,22 @@
+//Funçõs
+//Nomear uma função sempre um Verbo + Substantivo
+//Quandor for dar nome a uma função sempre ser claro e objetivo e escrever oq ela realmente faz
+
+// let corSite = "Azul"
+// function restaCor(cor,tonalidade){//dentro das aspas()temos os parametros ou entrada de dados 
+//   corSite = cor + ' ' + tonalidade ; //concatenei ou seja somei (+)
+// };
+// console.log(corSite);
+// restaCor("verde","Claro");
+// console.log(corSite)
+
+
+//Exercício que fiz!
+ marcaCarro = "Fiat"
+function mudarMarca(marca,cor){
+   marcaCarro = marca + " " + cor;
+}
+
+console.log(marcaCarro)
+mudarMarca("BMW","Amarela");
+console.log(marcaCarro)
