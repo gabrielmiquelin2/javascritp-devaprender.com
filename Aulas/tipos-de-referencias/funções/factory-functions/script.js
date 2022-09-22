f2b2f1bbe0c64465b80a80d882675f18950a625e
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-09-22 16:35:48 -0300

    Update script.js

--- Aulas/tipos-de-referencias/funções/factory-functions/script.js
+++ Aulas/tipos-de-referencias/funções/factory-functions/script.js
@@ -1,7 +1,7 @@
 //Factory functions  ou função de fábrica serve para vc otmizar a criação de objetos,por exemplo se quero cadastra 100 celulares a partir do objeto ou seja eu iria precisar de 100 estrutras de objetos como está aqui embaixo o FACTORY FUNCTIONS otmiza isso para não precisar digitar 100 vezes a mesma coisa e ir alterando a marca,preço etc...
 
 const celular ={
-  marcaCelular : "APPLE",
+  marcaCelular : "ASUS",
   tamanhoTela :{
     vertical: 155,
     horizontal: 75
@@ -28,5 +28,6 @@ function criarCelular(marcaCelular,tamanhoTela,capacidadeDaBateria){
    
 }
 
-const celular1 = criarCelular("Zenfone",5.5,5000);
+//Criação do método para criar o objeto
+const celular1 = criarCelular("Apple",5.5,5000);
 console.log(celular1)
