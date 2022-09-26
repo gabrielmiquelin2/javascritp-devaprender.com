d4614f1b615c5a8188229f1af320a2c0747f73e7
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-09-26 14:52:57 -0300

    commit

---
+++ Aulas/tipos-de-valores/tipos-de-string/template-literal/script.js
@@ -0,0 +1,26 @@
+//Template Literal
+
+//Tipos de literais
+//Object{}
+//Boolean true,false
+//string '',""
+//template `` //vai aparecer no console exatamento como escrevi exemplo:
+
+
+const nome = "Rafael"
+const mensagem = "Olá" + nome + '\n'//SEM template litera
+
+
+//COM template literal
+//vai aparecer no console exatamento como escrevi exemplo:
+const email =
+`Olá ${nome} ${2}
+
+Obrigado por me seguir no insagram
+
+para me acompanhar, ative as notificações
+
+obgriado,
+Lucas`;
+
+// \n = quebra de linha
